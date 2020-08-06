09de30924e2e8eb09df4764d402d04e82e7339df
Mykhailo Pechkurov <mykhailo.pechkurov@here.com>
2020-08-06 16:29:57 +0200

    Initial commit - implemented index page - added controller for search

---
+++ src/main/java/com/test/hplus/controllers/SearchController.java
@@ -0,0 +1,14 @@
+package com.test.hplus.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SearchController {
+
+    @GetMapping("/search")
+    public String search() {
+        System.out.println("I am in search controller");
+        return "search";
+    }
+}
