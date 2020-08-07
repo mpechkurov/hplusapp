91ce20821426446ce7600ae24c13af10bdabd796
Mykhailo Pechkurov <mykhailo.pechkurov@here.com>
2020-08-07 16:33:13 +0200

    Added implementation for search: - added mysql dump - added mapping data from controller to jsp files. - added logic to jsp files to display list of data.

--- src/main/java/com/test/hplus/controllers/SearchController.java
+++ src/main/java/com/test/hplus/controllers/SearchController.java
@@ -1,17 +1,31 @@
 package com.test.hplus.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.test.hplus.beans.Product;
+import com.test.hplus.repository.ProductRepository;
+
 @Controller
 public class SearchController {
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @GetMapping("/search")
     public String search(@RequestParam("search") String search, Model model) {
         System.out.println("I am in search controller");
         System.out.println("search criteria: " + search);
+
+        List<Product> products = new ArrayList<>();
+        products = productRepository.searchByName(search);
+        model.addAttribute("products", products);
         return "search";
     }
 }
