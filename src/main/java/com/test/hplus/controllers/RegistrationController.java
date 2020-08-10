715a24336dd4412a75cb18e3d283e47e98cb24af
Mykhailo Pechkurov <mykhailo.pechkurov@here.com>
2020-08-10 17:02:23 +0200

    Added validation for form fields - added validation for User bean fields - added validation handling in RegistrationController - added validation for register.jsp fields

--- src/main/java/com/test/hplus/controllers/RegistrationController.java
+++ src/main/java/com/test/hplus/controllers/RegistrationController.java
@@ -1,8 +1,11 @@
 package com.test.hplus.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,8 +18,11 @@ public class RegistrationController {
     private UserRepository userRepository;
 
     @PostMapping("/registeruser")
-    public String registerUser(@ModelAttribute("newuser") User user, Model model) {
+    public String registerUser(@Valid @ModelAttribute("newuser") User user, BindingResult result, Model model) {
         System.out.println("in registration controller");
+        if (result.hasErrors()) {
+            return "register";
+        }
         userRepository.save(user);
         model.addAttribute("dataSaved", "User registered successfully");
         return "login";
