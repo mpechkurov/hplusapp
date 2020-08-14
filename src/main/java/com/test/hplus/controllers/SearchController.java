884d22e7a466ec04bef777e7db97591983d2b38b
Mykhailo Pechkurov <mykhailo.pechkurov@here.com>
2020-08-14 15:27:57 +0200

    Added async support for application.

--- src/main/java/com/test/hplus/controllers/SearchController.java
+++ src/main/java/com/test/hplus/controllers/SearchController.java
@@ -2,6 +2,9 @@ package com.test.hplus.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,13 +22,20 @@ public class SearchController {
     private ProductRepository productRepository;
 
     @GetMapping("/search")
-    public String search(@RequestParam("search") String search, Model model) {
+    public Callable<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
         System.out.println("I am in search controller");
         System.out.println("search criteria: " + search);
+        System.out.println("Async supported in application:" + request.isAsyncSupported());
+        System.out.println("Thread from the servlet container: " + Thread.currentThread().getName());
 
-        List<Product> products = new ArrayList<>();
-        products = productRepository.searchByName(search);
-        model.addAttribute("products", products);
-        return "search";
+        return () -> {
+            //Just to simulate delay of a blocked call 3 sec
+            Thread.sleep(3000);
+            System.out.println("Thread from the spring MVC task executor: " + Thread.currentThread().getName());
+            List<Product> products = new ArrayList<>();
+            products = productRepository.searchByName(search);
+            model.addAttribute("products", products);
+            return "search";
+        };
     }
 }
