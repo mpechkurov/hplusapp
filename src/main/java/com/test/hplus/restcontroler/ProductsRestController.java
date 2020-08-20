506ed26c2a45e03aad815b9c02a73405e5a8df16
Mykhailo Pechkurov <mykhailo.pechkurov@here.com>
2020-08-20 17:27:44 +0200

    Change controller to rest controller add 2 methods  which will accept: - request parameter - path variable

--- src/main/java/com/test/hplus/restcontroler/ProductsRestController.java
+++ src/main/java/com/test/hplus/restcontroler/ProductsRestController.java
@@ -1,28 +1,47 @@
 package com.test.hplus.restcontroler;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.test.hplus.beans.Product;
 import com.test.hplus.repository.ProductRepository;
 
-@Controller
+@RestController
 public class ProductsRestController {
 
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/hplus/rest/products")
+    /*@GetMapping("/hplus/rest/products")
     @ResponseBody
     public List<Product> getProducts() {
         //call product repository to get data
         List<Product> products = new ArrayList<>();
         productRepository.findAll().forEach(product -> products.add(product));
         return products;
+    }*/
+
+    // Will accept request param
+    //    http://localhost:8080/hplus/rest/products?name=water
+    @GetMapping("/hplus/rest/products")
+    public ResponseEntity getProductsByRequestParam(@RequestParam("name") String name) {
+        List<Product> products = productRepository.searchByName(name);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
+
+    // Will accept path variable
+    //    http://localhost:8080/hplus/rest/products/water
+    @GetMapping("/hplus/rest/products/{name}")
+    public ResponseEntity getProductsByPathVariable(@PathVariable("name") String name) {
+        List<Product> products = productRepository.searchByName(name);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
 }
