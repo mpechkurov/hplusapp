307410afc696dffc773b8927a4a936542a86ad4d
Mykhailo Pechkurov <mykhailo.pechkurov@here.com>
2020-08-20 17:16:01 +0200

    Introduce rest controller for products.

---
+++ src/main/java/com/test/hplus/restcontroler/ProductsRestController.java
@@ -0,0 +1,28 @@
+package com.test.hplus.restcontroler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.test.hplus.beans.Product;
+import com.test.hplus.repository.ProductRepository;
+
+@Controller
+public class ProductsRestController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/hplus/rest/products")
+    @ResponseBody
+    public List<Product> getProducts() {
+        //call product repository to get data
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(product -> products.add(product));
+        return products;
+    }
+}
